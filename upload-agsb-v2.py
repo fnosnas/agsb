b9bc083d019c50126a33a6c5bb733e56d757a310
fnosnas <fnosnas@outlook.com>
2025-06-06 21:19:07 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
+USER_NAME = "fnos9527"         # 用户名
 UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
 PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+DOMAIN = "idx.afnos86.dpdns.org"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiZjQ4ODU5Y2IwODkzYjU2YWRhMzE4MjhkNDA5NjlmYTIiLCJ0IjoiMDUyZWUzMjItNjdlZi00NTRiLTk3YWUtYWQ5Y2EzZGFiMzMxIiwicyI6IlptVmtNamhtWmpndE5UUXdZeTAwWkRVNExXRTVOR0V0WkRSbU56VTJZV00zT1RjdyJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
